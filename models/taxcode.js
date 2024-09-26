e0e18aa4719bd199d626296f4a943473e495e5e0
Leopold Mathias Regner <office@leoregner.eu>
2024-09-26 09:25:18 +0200

    make tax code may require a specific currency

--- models/taxcode.js
+++ models/taxcode.js
@@ -24,6 +24,7 @@ const TaxCode = mongoose.model("TaxCode", (function()
         description: String,
         keywords: [ String ],
         tax_base: { type: String, enum: [ "net", "gross" ], default: "net", required: true },
+        currency: String,
         rates: [ Number ],
         type: { type: String, enum: [ "tax payable", "input tax receivable", "purchase tax payable and receivable", "tax payment" ], required: true },
         un_ece_5305: String, // https://unece.org/fileadmin/DAM/trade/untdid/d16b/tred/tred5305.htm
