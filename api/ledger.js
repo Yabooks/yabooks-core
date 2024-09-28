2c7ac0abe979f734cd30976631311176bc12f0c8
Leopold Mathias Regner <leopold.mathias.regner@gmail.com>
2024-09-28 16:20:34 +0200

    improved tax balances

--- api/ledger.js
+++ api/ledger.js
@@ -115,23 +115,98 @@ module.exports = function(api)
         catch(x) { next(x) }
     });
 
-    api.get("/api/v1/businesses/:id/tax-balances", async (req, res, next) =>
+    api.get("/api/v1/businesses/:id/tax-balances", async (req, res, next) => // ?from=&to=
     {
         try
         {
+            let req_currency = null;
+            if(req.query.currency) {
+                req_currency = req.query.currency;
+                req.query.currency = undefined;
+            }
+
+            const useAppropriateCurrency = function(default_currency, amount, alternate_currency, alternate_currency_amount, req_currency, tax_code_currency, tx_id)
+            {
+                if(req_currency)
+                {
+                    if(default_currency == req_currency)
+                        return amount;
+                    if(alternate_currency == req_currency)
+                        return alternate_currency_amount;
+                    return `value of transaction ${tx_id} is not available in ${req_currency}`+JSON.stringify(arguments);
+                }
+
+                if(tax_code_currency)
+                {
+                    if(default_currency == tax_code_currency)
+                        return amount;
+                    if(alternate_currency == tax_code_currency)
+                        return alternate_currency_amount;
+                    return `value of transaction ${tx_id} is not available in ${tax_code_currency}`;
+                }
+
+                return amount;
+            };
+
             res.send(await req.paginatedAggregatePipelineWithFilters(Document,
             [
-                { $match: { business: new mongoose.Types.ObjectId(req.params.id), posted: true } },
+                // get all tax ledger transactions
+                { $match: {
+                    business: new mongoose.Types.ObjectId(req.params.id),
+                    posted: true } }, // TODO from, to
+                { $lookup: { from: Business.collection.collectionName, localField: "business", foreignField: "_id", as: "business" } },
+                    { $unwind: { path: "$business", preserveNullAndEmptyArrays: true } },
                 { $unwind: "$ledger_transactions" },
-                { $set: { "tax_code": "$ledger_transactions.tax_code", "tax_percent": "$ledger_transactions.tax_percent", "tax_base": 0, "tax": "$ledger_transactions.amount" } },
+                { $match: { "ledger_transactions.tax_code": { $ne: null } } },
+                { $set: {
+                    "date": "$ledger_transactions.posting_date",
+                    "default_currency": "$business.default_currency",
+                    "alternate_currency": "$ledger_transactions.alternate_currency",
+                    "tax_code": "$ledger_transactions.tax_code",
+                    "tax_percent": "$ledger_transactions.tax_percent",
+                    "tax_base": 0,
+                    "tax_base_alternate_currency": 0,
+                    "tax": "$ledger_transactions.amount",
+                    "tax_alternate_currency": "$ledger_transactions.alternate_currency_amount" } },
+
+                // union with all tax-base transactions
                 { $unionWith: { coll: Document.collection.collectionName, pipeline: [
-                    { $match: { business: new mongoose.Types.ObjectId(req.params.id), posted: true } },
+                    { $match: {
+                        business: new mongoose.Types.ObjectId(req.params.id),
+                        posted: true } }, // TODO from, to
+                    { $lookup: { from: Business.collection.collectionName, localField: "business", foreignField: "_id", as: "business" } },
+                        { $unwind: { path: "$business", preserveNullAndEmptyArrays: true } },
                     { $unwind: "$ledger_transactions" },
-                    { $set: { "tax_code": "$ledger_transactions.tax_code_base", "tax_percent": "$ledger_transactions.tax_percent", "tax_base": "$ledger_transactions.amount", "tax": 0 } }
+                    { $match: { "ledger_transactions.tax_code_base": { $ne: null } } },
+                    { $set: {
+                        "date": "$ledger_transactions.posting_date",
+                        "default_currency": "$business.default_currency",
+                        "alternate_currency": "$ledger_transactions.alternate_currency",
+                        "tax_code": "$ledger_transactions.tax_code_base",
+                        "tax_percent": "$ledger_transactions.tax_percent",
+                        "tax_base": "$ledger_transactions.amount",
+                        "tax_base_alternate_currency": "$ledger_transactions.alternate_currency_amount",
+                        "tax": 0,
+                        "tax_alternate_currency": 0 } }
                 ] } },
-                { $group: { _id: { "tax_code": "$tax_code", "tax_percent": "$tax_percent" }, tax: { $sum: "$tax" }, tax_base: { $sum: "$tax_base"} } },
+
+                // convert currency if necessary
+                { $set: {
+                    //currency: { $ifNull: [ req_currency, null, "$default_currency" ] }, // TODO tax code currency
+                    tax: { $function: { body: useAppropriateCurrency, lang: "js", args: [
+                        "$default_currency", "$tax",
+                        "$alternate_currency", "$tax_alternate_currency",
+                        req_currency, null, "$ledger_transactions._id" ] } }, // TODO tax code currency
+                    tax_base: { $function: { body: useAppropriateCurrency, lang: "js", args: [
+                        "$default_currency", "$tax_base",
+                        "$alternate_currency", "$tax_base_alternate_currency",
+                        req_currency, null, "$ledger_transactions._id" ] } } } }, // TODO tax code currency
+
+                // group by tax code and format response
+                { $group: { _id: { "tax_code": "$tax_code", "tax_percent": "$tax_percent" },
+                    tax: { $sum: "$tax" },//{"$cond": { if: { $or: [ { "$not": { "$isNumber": "$tax" } }, { "$eq": [ "$tax", null ] } ] } } } , then: null, else: "$tax" },
+                    tax_base: { $sum: "$tax_base"} } }, // TODO sub tax code
                 { $set: { "tax_code": "$_id.tax_code", "tax_percent": "$_id.tax_percent" } },
-                { $match: { "tax_code": { $ne: null } } },
                 { $project: { _id: 0 } }
             ]));
         }
