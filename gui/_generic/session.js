fb34fbd115afb0acb1bced2ccf5f8235cd0bb67f
Leopold Mathias Regner <leopold.mathias.regner@gmail.com>
2025-01-06 21:57:29 +0100

    load default page based on url

--- gui/_generic/session.js
+++ gui/_generic/session.js
@@ -27,22 +27,23 @@ const getUserLanguage = () =>
 // allow apps in iFrames to ask for the user's session token and altering the page URL
 window.addEventListener("message", async (event) =>
 {
-    if(event.source === frames.main && event.data === "what_is_user_session_token") // FIXME make sure that only login page receives the token
+    /*if(event.source === frames.main && event.data === "what_is_user_session_token")
         try
         {
             let session = await loadSession();
             console.info("app in iframe asked for user token, replying");
             event.source.postMessage({ user_token: session.user_token }, "*");
+            // FIXME make sure that only login page receives the token
         }
         catch(x)
         {
             console.error("could not send message to app", x);
-        }
+        }*/
 
     if([ window, frames.main ].includes(event.source) && event.data?.main_url)
     {
         let url = event.data.main_url.split("http://").join("").split("https://").join("");
         if(url.indexOf(self.location.host) === 0) url = url.substring(self.location.host.length);
-        self.location.hash = url;
+        history.pushState(null, null, `#${url}`);
     }
 });
