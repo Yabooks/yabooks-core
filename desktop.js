ae8092f6d0eb69226e57d12599230100af9c3cf4
Leopold Mathias Regner <leopold.mathias.regner@gmail.com>
2025-01-07 21:53:19 +0100

    #2

---
+++ desktop.js
@@ -0,0 +1,55 @@
+const { app, BrowserWindow, ipcMain } = require("electron");
+
+// start YaBooks core
+require("./index.js");
+
+// display web app in electron window
+let mainWindow = null;
+app.whenReady().then(_ =>
+{
+    mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 650,
+        title: "YaBooks",
+        autoHideMenuBar: true,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+
+    mainWindow.setMenu(null);
+    mainWindow.on("closed", _ => mainWindow = null);
+    mainWindow.loadURL(process.env.base_url || `http://localhost:${process.env.port}`);
+
+    // manipulate local storage to enable auto-logon
+    /*mainWindow.webContents.on("did-finish-load", async () =>
+    {
+        try {
+            await mainWindow.webContents.executeJavaScript(`localStorage.setItem("key", "value_from_main_process");`);
+        }
+        catch(x) {}
+    });*/
+
+    // open another browser window if app is reactivated on macOS
+    app.on("activate", () =>
+    {
+        if(BrowserWindow.getAllWindows().length === 0)
+            arguments.callee();
+    });
+});
+
+// quit application if all windows are closed, except for macOS
+app.on("window-all-closed", () =>
+{
+    if(process.platform !== "darwin") {
+        app.quit();
+        process.exit();
+    }
+});
+
+// allow web app to communicate
+/*ipcMain.on("", (event, args) =>
+{
+    console.log(event, args);
+});*/
