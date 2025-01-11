09dbc75329bb2f35b77f979347243967fb0c120d
Leopold Mathias Regner <leopold.mathias.regner@gmail.com>
2025-01-11 15:28:44 +0100

    #5 #6

--- api/translation.js
+++ api/translation.js
@@ -2,11 +2,92 @@ const { FieldTranslation } = require("../models/translation.js");
 
 module.exports = function(api)
 {
+    /**
+     * @swagger
+     * /api/v1/translations:
+     *   get:
+     *     summary: Get all registered translations
+     *     description: Returns all registered string translations matching the request query
+     *     tags:
+     *       - translations
+     *     responses:
+     *       200:
+     *         description: Successful response
+     *         content:
+     *           application/json:
+     *             schema:
+     *                type: object
+     *                properties:
+     *                  data:
+     *                    type: array
+     *                    items:
+     *                      type: object
+     *                      properties:
+     *                        _id:
+     *                          type: string
+     *                          example: "507f191e810c19729de860ea"
+     *                          description: "Internal database ID, which shall not be used externally"
+     *                        code:
+     *                          type: string
+     *                          example: "com.example.greeting"
+     *                          description: "Unique identifier of the string with a prefix indicating its context"
+     *                        language:
+     *                          type: string
+     *                          example: "de-US"
+     *                          description: "Language of the translation as ISO code"
+     *                        text:
+     *                          type: string
+     *                          example: "Hello"
+     *                          description: "The actual translation in the language of desire"
+     *                        __v:
+     *                          type: integer
+     *                          example: 0
+     *                          description: "Version number of the translation"
+     */
     api.get("/api/v1/translations", async (req, res, next) =>
     {
         try
         {
             res.send(await req.paginatedAggregatePipelineWithFilters(FieldTranslation));
+        }
+        catch(x) { next(x) }
+    });
+
+    /**
+     * @swagger
+     * /api/v1/translations/languages:
+     *   get:
+     *     summary: Get all registered languages
+     *     description: Returns a distinct list of all lanuages, for which translations are registered
+     *     tags:
+     *       - translations
+     *     responses:
+     *       200:
+     *         description: Successful response
+     *         content:
+     *           application/json:
+     *             schema:
+     *                type: object
+     *                properties:
+     *                  data:
+     *                    type: array
+     *                    items:
+     *                      type: object
+     *                      properties:
+     *                        language:
+     *                          type: string
+     *                          example: "de-US"
+     *                          description: "Language as ISO code"
+     */
+    api.get("/api/v1/translations/languages", async (req, res, next) =>
+    {
+        try
+        {
+            res.send(await req.paginatedAggregatePipelineWithFilters(FieldTranslation, [
+                { $group: { _id: "$language" } },
+                { $project: { _id: 0, language: "$_id" } },
+                { $sort: { language: 1 } }
+            ]));
         }
         catch(x) { next(x) }
     });
