69347930fb8d1bfc5b8f0319f40ade9e10bea68b
Leopold Mathias Regner <leopold.mathias.regner@gmail.com>
2025-08-22 20:33:52 +0200

    #19

--- models/notification.js
+++ models/notification.js
@@ -5,11 +5,14 @@ const Notification = mongoose.model("Notification",
 {
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     title: { type: String, required: true },
+    tags: [ String ],
     text: String,
     icon: String,
     link: String,
     read: Date,
+    business: { type: mongoose.Schema.Types.ObjectId, ref: "Business", required: false },
     type: { type: String, enum: [ "app_notification", "user_notification", "user_task" ], default: "user_notification" },
+    task_status: { type: String, default: "new" },
     owned_by: { type: mongoose.Schema.Types.ObjectId, ref: "App" }
 });
 
