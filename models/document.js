fca32b89fc6f398436e7b72710ae699af0cba452
Leopold Mathias Regner <leopold.mathias.regner@gmail.com>
2025-10-27 21:41:04 +0100

    #21

--- models/document.js
+++ models/document.js
@@ -126,74 +126,6 @@ const CostTransaction = (function()
     return schema;
 })();
 
-// time log schema
-const TimeTransaction = (function()
-{
-    const schemaDefinition = (
-    {
-        posting_date: { type: Date, required: true, default: Date.now, get: omitTimezone, set: omitTimezone },
-        staff: { type: mongoose.Schema.Types.ObjectId, ref: "Identity", required: true },
-        cost_center: { type: mongoose.Schema.Types.ObjectId, ref: "CostCenter" },
-        minutes: { type: Number, required: true, default: 0 },
-        value: { type: mongoose.Schema.Types.Decimal128, required: true, default: 0 },
-        time_start: Date,
-        time_end: Date
-    });
-
-    let schema = new mongoose.Schema(schemaDefinition, { id: false, toJSON: { virtuals: true } });
-    schema.path("staff").index(true);
-    schema.path("cost_center").index(true);
-    schema.virtual("hourly_rate").get(function() { return -this.value / this.minutes * 60; });
-    return schema;
-})();
-
-// stock transaction schema
-const StockTransaction = (function()
-{
-    const schemaDefinition = (
-    {
-        posting_date: { type: Date, required: true, default: Date.now, get: omitTimezone, set: omitTimezone },
-        article: { type: mongoose.Schema.Types.ObjectId, ref: "Article", required: true },
-        store: { type: mongoose.Schema.Types.ObjectId, ref: "Store", required: true },
-        amount: { type: mongoose.Schema.Types.Decimal128, required: true },
-        unit: String,
-        value: { type: mongoose.Schema.Types.Decimal128, required: true, default: 0 }
-    });
-
-    let schema = new mongoose.Schema(schemaDefinition, { id: false, toJSON: { virtuals: true } });
-    schema.path("article").index(true);
-    schema.path("store").index(true);
-    return schema;
-})();
-
-// shipping transaction schema
-const ShippingTransaction = (function()
-{
-    const schemaDefinition = (
-    {
-        posting_date: { type: Date, required: true, default: Date.now, get: omitTimezone, set: omitTimezone },
-        text: String,
-        kn8_code: String,
-        hts_code: String,
-        mode_of_transport: Number,
-        tax_number: String,
-        incoterms: { type: String, validate: { validator: (v) => /^[A-Z]{3}$/.test(v) } },
-        origin: { type: String, validate: { validator: (v) => /^[A-Z]{2}(\-.+)?$/.test(v) } },
-        departure: { type: String, validate: { validator: (v) => /^[A-Z]{2}(\-.+)?$/.test(v) } },
-        destination: { type: String, validate: { validator: (v) => /^[A-Z]{2}(\-.+)?$/.test(v) } },
-        weight: Number,
-        value: { type: mongoose.Schema.Types.Decimal128, required: true, default: 0 }
-    });
-
-    let schema = new mongoose.Schema(schemaDefinition, { id: false, toJSON: { virtuals: true } });
-    schema.path("kn8_code").index(true);
-    schema.path("hts_code").index(true);
-    schema.path("origin").index(true);
-    schema.path("departure").index(true);
-    schema.path("destination").index(true);
-    return schema;
-})();
-
 const Payment = (function()
 {
     const schemaDefinition = (
@@ -237,9 +169,6 @@ const Document = mongoose.model("Document", (function()
 
         ledger_transactions: [ LedgerTransaction ],
         cost_transactions: [ CostTransaction ],
-        time_transactions: [ TimeTransaction ],
-        stock_transactions: [ StockTransaction ],
-        shipping_transactions: [ ShippingTransaction ],
 
         classification: { type: String, enum: [ "top secret", "secret", "confidential", "restricted", "official" ], default: "restricted" },
         created_by: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // may be null if document was created by an app
