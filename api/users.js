0bde958a06924d1e9249c16ae319a83b60fc6650
Leopold Mathias Regner <leopold.mathias.regner@gmail.com>
2026-09-22 23:53:35 +0200

    user list api endpoint

--- api/users.js
+++ api/users.js
@@ -2,6 +2,53 @@ const { User, Session } = require("../models/user.js");
 
 module.exports = function(api)
 {
+    /**
+     * @openapi
+     * /api/v1/users:
+     *   get:
+     *     summary: List all users
+     *     description: >
+     *       Returns a paginated list of users with their email address and, if linked, the name of their
+     *       individual identity as `full_name`. Credentials (password hash, authenticator key, external
+     *       auth info) are never included. Supports filtering and sorting via query parameters.
+     *     tags:
+     *       - users
+     *     responses:
+     *       200:
+     *         description: Paginated list of users
+     *         content:
+     *           application/json:
+     *             schema:
+     *               type: object
+     *               allOf:
+     *                 - $ref: '#/components/schemas/PaginatedResponse'
+     *                 - properties:
+     *                     data:
+     *                       type: array
+     *                       items:
+     *                         type: object
+     *                         properties:
+     *                           _id: { type: string }
+     *                           email: { type: string }
+     *                           preferred_language: { type: string }
+     *                           individual: { type: string }
+     *                           full_name: { type: string }
+     */
+    api.get("/api/v1/users", async (req, res, next) =>
+    {
+        try
+        {
+            res.send(await req.paginatedAggregatePipelineWithFilters(User, [
+                { $lookup: { from: "identities", localField: "individual", foreignField: "_id", as: "individual_identity" } },
+                { $project: {
+                    email: 1, preferred_language: 1, individual: 1,
+                    full_name: { $arrayElemAt: [ "$individual_identity.full_name", 0 ] }
+                } }
+            ]));
+        }
+        catch(x) { next(x) }
+    });
+
     // get details of a user profile
     api.get("/api/v1/users/:id", async (req, res, next) =>
     {
